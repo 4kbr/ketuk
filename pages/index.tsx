100f437c7fff90b5b60cc1f1fa99cd477d939b3e
4kbr <igyutubfesbuk@gmail.com>
2023-01-01 23:54:19 +0700

    adding tailwind and starting header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import Head from "next/head";
+import Header from "../components/Header";
 
 const home = () => {
-  return <div>Test</div>;
+  return (
+    <div>
+      <Head>
+        <title>Keenook</title>
+        <meta name="description" content="Search Engine UWU" />
+        <link rel="icon" href="/favicon.icon" />
+      </Head>
+      {/* Header */}
+      <Header />
+      {/* Close Header */}
+
+      {/* Body */}
+      {/* Close Body */}
+
+      {/* Footer */}
+      {/* Close Footer */}
+    </div>
+  );
 };
 
 export default home;
