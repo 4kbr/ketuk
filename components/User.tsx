cb2ac55c284b2a76502d9da4bdfa043e89ee6710
akbar-human <akbar@hub9.biz>
2023-01-02 18:01:03 +0700

    started signIn and signOut

--- components/User.tsx
+++ components/User.tsx
@@ -1,5 +1,13 @@
-import React from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function User() {
-  return <div>User</div>;
+  const { data: session } = useSession();
+  if (session) {
+    return <>Sign Out</>;
+  }
+  return (
+    <>
+      <button onClick={signIn}>Sign In</button>
+    </>
+  );
 }
--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,23 +1,43 @@
-import React from 'react'
-import Header from '../../components/Header'
-import {ClientSafeProvider, getProviders, LiteralUnion} from "next-auth/react";
-import { GetServerSideProps, NextPage } from 'next';
-import { BuiltInProviderType } from 'next-auth/providers';
+import React from "react";
+import Header from "../../components/Header";
+import {
+  ClientSafeProvider,
+  getProviders,
+  LiteralUnion,
+} from "next-auth/react";
+import { GetServerSideProps, NextPage } from "next";
+import { BuiltInProviderType } from "next-auth/providers";
 
-export default function Signin ({providers}:{providers :Record<LiteralUnion<BuiltInProviderType, string>, ClientSafeProvider>|null}) {
+export default function Signin({
+  providers,
+}: {
+  providers: Record<
+    LiteralUnion<BuiltInProviderType, string>,
+    ClientSafeProvider
+  > | null;
+}) {
   return (
     <>
-    <Header/>
-    <div className=''>
- 
-    </div>
+      <Header />
+      <div className="">
+        {Object.values(providers!).map((provider, i) => (
+          <div key={i} className="">
+            <img
+              src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-google-sva-scholarship-20.png"
+              alt="ketuk - google clone"
+            />
+            <p>This website is created for learning purposes</p>
+            <button>Sign in with {provider.name}</button>
+          </div>
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export async function getServerSideProps () {
+export async function getServerSideProps() {
   const providers = await getProviders();
   return {
-    props:{providers},
+    props: { providers },
   };
 }
