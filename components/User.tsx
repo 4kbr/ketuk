84a64dfb311687fe76eb7301917feadb22b164da
akbar-human <akbar@hub9.biz>
2023-01-03 11:20:04 +0700

    adding next auth for google and sigin layout

--- components/User.tsx
+++ components/User.tsx
@@ -2,12 +2,30 @@ import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function User() {
   const { data: session } = useSession();
+  console.log(session);
+
   if (session) {
-    return <>Sign Out</>;
+    return (
+      <>
+        <img
+          className="h-10 h-10 rounded-full hover:bg-gray-100 cursor-pointer p-1"
+          src={session.user!.image!}
+          alt={"KETUK USER - " + session.user?.name}
+        />
+        <button className="bg-blue-400 p-4" onClick={() => signOut()}>
+          Sign Out
+        </button>
+      </>
+    );
   }
   return (
     <>
-      <button onClick={signIn}>Sign In</button>
+      <button
+        className="bg-blue-400 px-3 py-1 rounded-md text-white hover:bg-blue-100 "
+        onClick={() => signIn()}
+      >
+        Sign In
+      </button>
     </>
   );
 }
--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,12 +1,14 @@
-import React from "react";
-import Header from "../../components/Header";
+/* eslint-disable @next/next/no-img-element */
 import {
   ClientSafeProvider,
   getProviders,
   LiteralUnion,
+  signIn,
 } from "next-auth/react";
-import { GetServerSideProps, NextPage } from "next";
+import Header from "../../components/Header";
 import { BuiltInProviderType } from "next-auth/providers";
+import Head from "next/head";
+import Image from "next/image";
 
 export default function Signin({
   providers,
@@ -18,16 +20,29 @@ export default function Signin({
 }) {
   return (
     <>
+      <Head>
+        <title>KETUK - Sign In</title>
+      </Head>
       <Header />
-      <div className="">
+      <div className="mt-40">
         {Object.values(providers!).map((provider, i) => (
-          <div key={i} className="">
+          <div key={i} className="flex flex-col items-center">
             <img
-              src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-google-sva-scholarship-20.png"
-              alt="ketuk - google clone"
+              className="w-52 object-cover"
+              src={
+                "https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-google-sva-scholarship-20.png"
+              }
+              alt={"ketuk - google clone"}
             />
-            <p>This website is created for learning purposes</p>
-            <button>Sign in with {provider.name}</button>
+            <p className="italic text-sm my-10 text-center">
+              This website is created for learning purposes
+            </p>
+            <button
+              className="bg-red-300 p-3 rounded-lg text-white hover:bg-red-500 transition duration-300"
+              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+            >
+              Sign in with {provider.name}
+            </button>
           </div>
         ))}
       </div>
